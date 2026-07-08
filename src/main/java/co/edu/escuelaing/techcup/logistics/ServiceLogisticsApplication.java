792f1a220c554751194ef2c9f0980b74360dd84b
julianizz18 <904juliancorredor@gmail.com>
2026-07-07 23:48:01 -0500

    Realizacion del repo

---
+++ src/main/java/co/edu/escuelaing/techcup/logistics/ServiceLogisticsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLogisticsApplication.class, args);
+	}
+
+}
